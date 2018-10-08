b2ff313e0141a554f0597d92a9e92a88aa01a4fe
sergeklokov <serge.klokov@gmail.com>
2018-10-07 23:08:07 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObservableDesignPattern
+{
+    class Observable
+    {
+        public event EventHandler SomethingHappened;
+
+        public void DoSomething()
+        {
+            EventHandler handler = SomethingHappened;
+            if (handler != null)
+            {
+                handler(this, EventArgs.Empty);
+            }
+        }
+
+    }
+
+    class Observer
+    {
+        public void HandleEvent(object sender, EventArgs args)
+        {
+            Console.WriteLine("Something happened to " + sender);
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Observable observable = new Observable();
+            Observer observer = new Observer();
+
+            observable.SomethingHappened += observer.HandleEvent;
+
+            observable.DoSomething();
+
+            Console.WriteLine("End of work, please press any key.");
+            Console.ReadKey();
+        }
+    }
+}
